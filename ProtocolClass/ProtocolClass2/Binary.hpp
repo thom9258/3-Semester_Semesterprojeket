53c63070d4c4b322e9e29f0aab0209e0ec9d8852
Mikkel <mikkel114@gmail.com>
2019-11-05 11:34:38 +0100

    Deleting bin

--- ProtocolClass/ProtocolClass2/Binary.hpp
+++ ProtocolClass/ProtocolClass2/Binary.hpp
@@ -1,17 +1,1 @@
-#include <stdio.h>
-#include <vector>
-#include <initializer_list>
 
-class Binary {
-    
-public:
-    Binary(std::initializer_list<int> base);
-    
-    std::vector<int> Append(Binary base, Binary append);
-    
-    
-    
-private:
-    std::vector<int> data;
-    
-};
