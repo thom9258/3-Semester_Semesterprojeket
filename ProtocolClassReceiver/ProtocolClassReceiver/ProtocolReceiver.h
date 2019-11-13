03cbf843992e0ea5003d7422d35ee9082a4f8fe6
Mikkel <mikkel114@gmail.com>
2019-11-13 10:07:19 +0100

    sd

--- ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
+++ ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
@@ -10,7 +10,22 @@ class ProtocolReceiver
 public:
 	ProtocolReceiver(std::vector<int> input) : FullFrame(input) {};
 
-	//check crc
+	std::string receiver(std::string senderCRC, std::string CRC)
+    {
+        for(int i=0; i<=senderCRC.length()-CRC.length(); ) {
+            for(int k=0; k<CRC.length(); k++)
+                senderCRC[i+k]=senderCRC[i+k]==CRC[k]? '0' : '1';
+                for(; i < senderCRC.length() && senderCRC[i]!='1'; i++);
+            }
+
+        for( char i: senderCRC.substr(senderCRC.length()-CRC.length()+1) )
+            if(i!='0'){
+                std::string final = "ERRRROOROOROR";
+                return final;
+            }
+        std::string final = "No error :)";
+                return final;
+    }
 
 	//RecNumbering
 
