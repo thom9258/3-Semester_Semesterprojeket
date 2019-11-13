60f13f30aa436a1bc6c3a386b092a36d34065f4b
Mikkel <mikkel114@gmail.com>
2019-11-13 08:46:23 +0100

    deleting cpp files

---
+++ ProtocolClassReceiver/ProtocolClassReceiver/BinaryReceiver.h
@@ -0,0 +1,13 @@
+//
+//  BinaryReceiver.h
+//  ProtocolClassReceiver
+//
+//  Created by Mikkel Werge Olsen on 13/11/2019.
+//  Copyright © 2019 Mikkel Werge Olsen. All rights reserved.
+//
+
+#ifndef BinaryReceiver_h
+#define BinaryReceiver_h
+
+
+#endif /* BinaryReceiver_h */
