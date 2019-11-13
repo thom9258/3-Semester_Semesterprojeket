4cfc270a341eb207d7d963ada64e863dbb6042a9
Mikkel <mikkel114@gmail.com>
2019-11-13 09:35:07 +0100

    sd

--- ProtocolClassReceiver/ProtocolClassReceiver/BinaryReceiver.h
+++ ProtocolClassReceiver/ProtocolClassReceiver/BinaryReceiver.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <initializer_list>
+#include <math.h>
+#include <cassert>
+#include <algorithm>
+#include <sstream>
+
 class BinaryReceiver
 {
 public:
--- ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
+++ ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
 class ProtocolReceiver
 {
 public:
