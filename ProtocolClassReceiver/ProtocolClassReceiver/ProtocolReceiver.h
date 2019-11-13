effb973ef748e2c4107ec5f63d61b76da89671a7
Mikkel <mikkel114@gmail.com>
2019-11-13 09:21:56 +0100

    sd

--- ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
+++ ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
@@ -1,13 +1,10 @@
-//
-//  ProtocolReceiver.h
-//  ProtocolClassReceiver
-//
-//  Created by Mikkel Werge Olsen on 13/11/2019.
-//  Copyright © 2019 Mikkel Werge Olsen. All rights reserved.
-//
+#pragma once
 
-#ifndef ProtocolReceiver_h
-#define ProtocolReceiver_h
-
-
-#endif /* ProtocolReceiver_h */
+class ProtocolReceiver
+{
+public:
+    
+    
+private:
+    
+};
