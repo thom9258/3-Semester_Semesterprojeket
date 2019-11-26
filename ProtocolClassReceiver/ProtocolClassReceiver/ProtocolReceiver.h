1b81034eaff3d81b4cba27820ea73bb132c4a160
Mikkel <mikkel114@gmail.com>
2019-11-26 19:59:44 +0100

    lk

--- ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
+++ ProtocolClassReceiver/ProtocolClassReceiver/ProtocolReceiver.h
@@ -6,78 +6,72 @@
 #include <iostream>
 
 
-vector <int> getVectorFlags(vector<int> v1);
-vector <int> getVectorLength(vector<int> v1);
-int convertBinaryToDecimal(vector <int> v1);
-int readFrom;
-int readTo;
 class ProtocolReceiver
 {
 public:
 	ProtocolReceiver(std::vector<int> input) : FullFrame(input) {};
-
-	int convertBinaryToDecimal(std::vector <int> v1);
-	int readFrom;
-	int readTo;
-	std::vector <int> getVectorLength(vector<int> v1) {
-		vector<int> numbersOfLetters;
-		for (int i = 0; i < 4; i++) {
-
-			numbersOfLetters.push_back(v1[i])
-		}
-		int n = convertBinaryToDecimal(numbersOfLetters);
-		return n;
-
-	}
-	std::vector <int> getVectorBitData(std::vector<int> v1, int readFrom, int readTo) {
-
-		std::vector <int> vectorBits;
-		for (int i = readFrom; i < readTo; i++) {
-
-			vectorBits.push_back(v1.at(i));
-		};
+    
+    int numberingRec(std::vector<int> input)
+    {
+        input = FullFrame;
+        std::vector<int> firstfour;
+        
+        for(int i = 0; i < 4; i++)
+        {
+            firstfour.push_back(input[i]);
+        }
+        
+        numbering = firstfour.at(0) * 8 + firstfour.at(1) * 4 + firstfour.at(2) * 2 + firstfour.at(3) * 1;
 
 
-		return vectorBits;
-	};
-
-
-
-
-	int convertBinaryToDecimal(vector <int> v1)
-	{
-		int n = v1.at(0) * 1 + v1.at(1) * 2 + v1.at(2) * 4 + v1.at(3) * 8;
-
-
-		return n;
-	}
-
-
-
-
+        return numbering+1;
+        }
+    
+    void flagdetermine(){
+        if (FullFrame.at(6) == 1) {
+            ul = 1;
+        }
+        
+        else {
+            ul = 0;
+        }
+        
+        if (FullFrame.at(7) == 1) {
+            sf = 1;
+        }
+        
+        else{
+            sf = 0;
+        }
+    }
+    
+    std::vector<int> getData()
+    {
+        return FullFrame;
+    }
+    
 	//check crc
     
+    int getsf(){
+        return sf;
+    }
 
-	//RecNumbering
-
-	//RecFlagDetermine
-	int ProtocolReceiver::getFlagbits(vector<int> vec getVectorBitData(std::vector<int> inputVector, 0, 4)) {
-		if(vec[4]=1){
-			sf = 1;
-		};
-		if (vec[3] = 1) {
-			uligeFlag = 1;
-		};
-
-	}
-	//ConvertData
-
+    int getul(){
+        return ul;
+    }
+    
+    std::vector<int> databytesDetermine()
+    {
+        FullFrame.erase(FullFrame.begin(), FullFrame.begin()+8);
+        
+        FullFrame.erase(FullFrame.end()-8, FullFrame.end());
+        
+        return FullFrame;
+    }
+    
 
 private:
-	std::vector <int> getVectorFlags(vector<int> v1);
-	std::vector <int> getVectorLength(vector<int> v1);
 	std::vector<int> FullFrame;
-	int sf;
-	int uligeFlag;
-    std::string final;
+    int numbering;
+    int sf = 0, ul = 0;
 };
