3d9cd3c9ea1f3159d28db237eef6ceb59ce51ab8
Mikkel <mikkel114@gmail.com>
2019-12-03 15:00:29 +0100

    sd

--- ProtocolClassReceiver/ProtocolClassReceiver/main.cpp
+++ ProtocolClassReceiver/ProtocolClassReceiver/main.cpp
@@ -79,7 +79,7 @@ int main()
 		printVec(currVec, "curr data");
 
 
-		std::string currMessage = p1.returnMessage(currVec);
+		std::string currMessage = p1.returnMessage();
 
 		fullMessage += currMessage;
 	}
