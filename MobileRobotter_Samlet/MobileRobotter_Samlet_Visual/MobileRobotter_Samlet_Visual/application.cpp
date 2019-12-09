efe497846180933da7f2b62815ad5a5157958d4d
Anton Marthedal <Antonmarthedal@gmail.com>
2019-12-09 11:19:12 +0100

    Change setState in main

--- MobileRobotter_Samlet/MobileRobotter_Samlet_Visual/MobileRobotter_Samlet_Visual/application.cpp
+++ MobileRobotter_Samlet/MobileRobotter_Samlet_Visual/MobileRobotter_Samlet_Visual/application.cpp
@@ -18,30 +18,42 @@ int application::getState()
 string application::sender()
 {
 	if (state == 1) {
-		string j, q, w, d;
-		cin >> j;
-		q = j;
-		getline(cin, j);
-		w = j;
-		d = q + w;
-		return d;
+		// check state to see if the user is allowed to send data .
+		// If state is 1 the function continue.
+		string input, firstWord, restOfWords, entireSentence;
+		cin >> input;
+		firstWord = input;
+		//takes the first word and saves it in q.
+		getline(cin, input);
+		//takes the rest of the words and saves them in w.
+		restOfWords = input;
+		// Putting all the words together and saves them in d.
+		entireSentence = firstWord + restOfWords;
+		return entireSentence;
 	}
 
 	else if (state == 0) {
+		// check state to see if the user is allowed to send data.
+		// If the state is 0 the user is not allowed to send.
 		cout << "Du kan ikke sende";
 	}
+	setState(0);
 }
 
-string application::receiver(string y)
+void application::receiver(string y)
 {
 	if (state == 0) {
-		setState(1);
+		// check state to see if the user is allowed to send data.
+		// If the state is 0 the user is  allowed to receive.
 		cout << y << endl;
-		return y;
+	
 	}
 	else if (state == 1) {
+		// check state to see if the user is allowed to send data.
+		// If the state is 1 the user is not allowed to receive.
 		cout << "Du kan ikke modtage";
 
 
 	}
+	setState(1);
 }
